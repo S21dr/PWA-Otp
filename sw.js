2769c621327908fb65f95753bbf75e44c7901588
S21dr <S21dr@users.noreply.github.com>
2025-01-31 23:37:35 +0000

    deploy: 8d828543bd766b3a52fc6f443a97c7442602c0f3

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open("pwa-cache").then((cache) => {
-            return cache.addAll(["/", "/index.html", "/offline.html"]);
+            return cache.addAll([
+                "/PWA-Otp/",
+                "/PWA-Otp/index.html",
+                "/PWA-Otp/offline.html"
+            ]);
         })
     );
+    self.skipWaiting();
 });
 
-// self.addEventListener("fetch", (event) => {
-//     event.respondWith(
-//         fetch(event.request).catch(() => caches.match("/offline.html"))
-//     );
-// });
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+// Перехват всех fetch-запросов
+self.addEventListener("fetch", (event) => {
+    if (event.request.headers.get("x-msw-bypass") === "true") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => caches.match("/PWA-Otp/offline.html"))
+    );
+});
