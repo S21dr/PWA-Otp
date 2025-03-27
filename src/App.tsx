c496ac3a0829aa5b8b15abee5c02a91ba273656b
Dmitry <adel.ernandies@mail.ru>
2025-03-27 17:22:50 +0300

    fix: add link to pwatestmvp.ru

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {FC, useEffect, useState} from "react";
-import {Box, Container, Typography} from "@mui/material";
+import {Box, Container, FormControl, FormControlLabel, Radio, RadioGroup, Typography} from "@mui/material";
 import BiometricSetup from "./components/BiometricSetup";
 import OTPDisplay from "./components/OTPDisplay";
 import BiometricLogIn from "./components/BiometricLogIn.tsx";
@@ -8,6 +8,12 @@ import {RootState, store} from "./store";
 const App: FC = () => {
     const [step, setStep] = useState<number>(1);
     const rawId = (store.getState() as RootState).idb.rawId
+
+    const [value, setValue] = useState('_self');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue((event.target as HTMLInputElement).value);
+    };
 
     useEffect(() => {
         if (rawId) {
@@ -20,6 +26,23 @@ const App: FC = () => {
             <Typography variant="h3" style={{textAlign: "center", color: "#148f2a"}}>
                 PWA OTP
             </Typography>
+
+            <div>
+                <a target={value} href="https://pwatestmvp.ru">pwatestmvp.ru</a>
+            </div>
+            <FormControl style={{marginBottom:64}}>
+                <RadioGroup
+                    aria-labelledby="demo-controlled-radio-buttons-group"
+                    name="controlled-radio-buttons-group"
+                    value={value}
+                    onChange={handleChange}
+                >
+                    <FormControlLabel value="_self" control={<Radio />} label="default" />
+                    <FormControlLabel value="_blank" control={<Radio />} label="blank" />
+                    <FormControlLabel value="_parent" control={<Radio />} label="parent" />
+                    <FormControlLabel value="_top" control={<Radio />} label="top" />
+                </RadioGroup>
+            </FormControl>
 
             <Box mt={5}>
                 {step === 1 && <BiometricSetup onComplete={() => setStep(2)}/>}
